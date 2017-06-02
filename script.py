bfe1f175de7674f13f18d6f730ff39848cb788e9
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2017-06-02 15:26:15 -0500

    Gateway

--- script.py
+++ script.py
@@ -19,6 +19,7 @@ app = Flask(__name__)
 CORS(app)
 # Auth vars
 
+"""
 vsd_ip = '192.168.0.20'
 api_url = "https://192.168.0.20:8443"
 username = "csproot"
@@ -30,7 +31,7 @@ api_url = "https://147.75.69.37:8443"
 username = "csproot"
 password = "csproot"
 enterprise = "csp"
-"""
+
 def setup_logging():
     pass
     #set_log_level(logging.DEBUG, logging.Streamhandler())
